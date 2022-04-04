d28a351612c45d83e6a95b694fbf732f0c846ae9
Jon Elorriaga <jelorria@c3r4s3.42urduliz.com>
2022-04-04 19:01:33 +0200

    se mueve

--- so_long.c
+++ so_long.c
@@ -1,19 +1,64 @@
 #include <mlx.h>
 #include <stdio.h>
+#include <unistd.h>
+
+typedef struct  s_program
+{
+    void	*mlx;
+    void	*win;
+	void	*imgZ;
+	void	*imgF;
+	int		x;
+	int		y;
+}               t_program;
+
+int key_event(int key_code, void *param)
+{
+	t_program *main = param;
+	printf("%d\n", key_code);
+	if (key_code == 1)
+	{
+		main->y += 70;
+		mlx_put_image_to_window(main->mlx, main->win, main->imgZ, main->x, main->y);
+		mlx_put_image_to_window(main->mlx, main->win, main->imgF, main->x, main->y -70);
+	}
+	if (key_code == 2)
+	{
+		main->x += 70;
+		mlx_put_image_to_window(main->mlx, main->win, main->imgZ, main->x, main->y);
+		mlx_put_image_to_window(main->mlx, main->win, main->imgF, main->x - 70, main->y);
+	}
+	if (key_code == 13) // PARRIBA
+	{
+		main->y += -70;
+		mlx_put_image_to_window(main->mlx, main->win, main->imgZ, main->x, main->y);
+		mlx_put_image_to_window(main->mlx, main->win, main->imgF, main->x, main->y + 70);
+	}
+	//mlx_put_image_to_window(main->mlx, main->win, main->imgF, 0, 0);
+	//mlx_put_image_to_window(main->mlx, main->win, main->imgZ, 0, 70);
+	return 0;
+}
 
 int	main()
 {
 	void	*mlx;
 	void	*mlx_win;
 	void	*img;
-	int	img_width;
-	int	img_height;
-	int	x;
-	int	y;
+	int		img_width;
+	int		img_height;
+	int		x;
+	int		y;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1400, 700, "Hello World");
 	img = mlx_xpm_file_to_image(mlx, "img/floor.xpm", &img_width, &img_height);
+
+	t_program main;
+	main.mlx = mlx;
+	main.win = mlx_win;
+	main.imgF = img;
+
+	// DIBUJAR FONDO
 	x = 0;
 	y = 0;
 	while (y < 700)
@@ -26,7 +71,16 @@ int	main()
 		x = 0;
 		y = y + 70;
 	}
-	img = mlx_xpm_file_to_image(mlx, "img/fox_front_70x70.xpm", &img_width, &img_height);
+
+	// EL ZORRO
+	img = mlx_xpm_file_to_image(mlx, "img/fox_front_70x70.xpm", &img_width, &img_height);	
+	
+	main.imgZ = img;
+
 	mlx_put_image_to_window(mlx, mlx_win, img, 0,  0);
+	main.x = 0;
+	main.y = 0;
+	mlx_key_hook(mlx_win, &key_event, &main);
+
 	mlx_loop(mlx);
 }
